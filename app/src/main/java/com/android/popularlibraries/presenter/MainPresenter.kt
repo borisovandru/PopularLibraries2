d8fcedf856f863c63f7e649f35d9a0e6ec3ea53b
boris <borisov21011986@rambler.ru>
2021-10-19 21:25:57 +0400

    Поменял Button на Counter и т.д.

--- app/src/main/java/com/android/popularlibraries/presenter/MainPresenter.kt
+++ app/src/main/java/com/android/popularlibraries/presenter/MainPresenter.kt
@@ -1,33 +1,23 @@
 package com.android.popularlibraries.presenter
 
 import com.android.popularlibraries.model.CountersModel
-import com.android.popularlibraries.view.MainView
+import com.android.popularlibraries.view.IMainView
 
-class MainPresenter(private val view: MainView) {
-
+class MainPresenter(private val viewI: IMainView) {
     private val model = CountersModel()
 
     //Архитектурная ошибка. В качестве практического задания -- исправить
-    fun counterClick(type: ButtonType) {
-        when (type) {
-            ButtonType.FIRST -> {
-                val nextValue = model.next(0)
-                view.setButtonText(ButtonType.FIRST, nextValue.toString())
-            }
-            ButtonType.SECOND,
-            -> {
-                val nextValue = model.next(1)
-                view.setButtonText(ButtonType.SECOND, nextValue.toString())
-            }
-            ButtonType.THIRD -> {
-                val nextValue = model.next(2)
-                view.setButtonText(ButtonType.THIRD, nextValue.toString())
-            }
+    fun counterClick(type: CounterType) {
+        val dataFromModel = when (type) {
+            CounterType.FIRST -> model.next(0)
+            CounterType.SECOND -> model.next(1)
+            CounterType.THIRD -> model.next(2)
         }
+        viewI.setButtonText(type, dataFromModel.toString())
     }
 }
 
-enum class ButtonType {
+enum class CounterType {
     FIRST,
     SECOND,
     THIRD
